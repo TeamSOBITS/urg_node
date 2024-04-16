df0f6e2dfaac7e5eb399b1bef5e2c94b70b1a779
MrKeith99 <kvalentincardenas@hotmail.com>
2024-04-16 13:11:38 +0900

    [UPD] add laser-proc as dependency

--- install.sh
+++ install.sh
@@ -6,7 +6,8 @@ echo "╔══╣ Install: URG Node (STARTING) ╠══╗"
 # Install ROS packages
 sudo apt-get update
 sudo apt-get install -y \
-    ros-${ROS_DISTRO}-urg-c
+    ros-${ROS_DISTRO}-urg-c \
+    ros-${ROS_DISTRO}-laser-proc
 
 
 echo "╚══╣ Install: URG Node (FINISHED) ╠══╝"
