6cb469b6dddd7be1344d3197b20e40338d9ea40e
SUZUKI Koshiro <ksuzuki9541@gmail.com>
2025-09-01 15:55:12 +0900

    [FIX] install.sh

--- install.sh
+++ install.sh
@@ -8,9 +8,7 @@ sudo apt-get update
 sudo apt-get install -y \
     ros-${ROS_DISTRO}-urg-c \
     ros-${ROS_DISTRO}-laser-proc \
-    ros-${ROS_DISTRO}-urg-node-msgs
+    ros-${ROS_DISTRO}-urg-node-msgs \
     ros-${ROS_DISTRO}-rosidl-typesupport-c
-
-
 
 echo "╚══╣ Install: URG Node (FINISHED) ╠══╝"
