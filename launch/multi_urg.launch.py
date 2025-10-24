e7990a6a60b7ecb6595b0030924cd81ecf4907e5
OnoFumiya <fumiyaono.choi@gmail.com>
2025-10-24 10:46:46 +0900

    Add multi lidar launch file

---
+++ launch/multi_urg.launch.py
@@ -0,0 +1,50 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import OpaqueFunction
+from launch.actions import SetLaunchConfiguration
+from launch.substitutions import LaunchConfiguration
+from launch.conditions import IfCondition, UnlessCondition
+
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    launch_description = LaunchDescription()
+
+    namespace_cmd = DeclareLaunchArgument(
+        'namespace',
+        default_value='laser'
+    )
+
+    lidar_num_cmd = DeclareLaunchArgument(
+        'lidar_num',
+        default_value='2'
+    )
+
+    launch_description.add_action(namespace_cmd)
+    launch_description.add_action(lidar_num_cmd)
+    launch_description.add_action(OpaqueFunction(function = multi_node_launch))
+    return launch_description
+
+
+def multi_node_launch(context, *args, **kwargs):
+    namespace = LaunchConfiguration('namespace').perform(context)
+    lidar_num = LaunchConfiguration('lidar_num').perform(context)
+
+    config_files_cmd = []
+    for i in range(int(lidar_num)):
+        config_files_cmd += [DeclareLaunchArgument('config_file' + str(i+1), default_value=os.path.join(get_package_share_directory('urg_node'), 'config', "urg_node_multi_ethernet" + str(i+1) + ".yaml"))]
+
+    node_list = []
+    for i in range(int(lidar_num)):
+        node_list += [Node(
+            package='urg_node', namespace=namespace, executable='urg_node_driver', name='urg_node'+str(i+1), output='screen',
+            remappings=[('/' + namespace + '/scan', '/' + namespace + '/scan' + str(i+1))],
+            parameters=[LaunchConfiguration('config_file' + str(i+1))],
+        )]
+    
+    return config_files_cmd + node_list
